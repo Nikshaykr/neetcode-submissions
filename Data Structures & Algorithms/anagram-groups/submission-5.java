9ccea89069b59594b4abbd1ba542bf7e4a53043e
Nikshaykr <nikshaykr56@gmail.com>
2026-06-01 14:17:41 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (var word : strs) {
+            char[] sWord = word.toCharArray();
+            Arrays.sort(sWord);
+
+            String key = Arrays.toString(sWord);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
